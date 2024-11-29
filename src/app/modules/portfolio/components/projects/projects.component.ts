cb3165ed08f4dd350e9e3b5a52b165d60fd69e21
lucasfmerino <lucasfmerino@gmail.com>
2024-11-29 12:38:18 -0300

    test: added new project with new image

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -17,12 +17,12 @@ export class ProjectsComponent {
 
   public projects: WritableSignal<IProjectDetails[]> = signal<IProjectDetails[]>([
     {
-      src: '/assets/img/projects/hmdl-logo.png',
+      src: '/assets/img/portfolio/heimd4ll.png',
       alt: 'Projeto Heimd4ll',
       title: 'Heimd4ll - Inteligência em Dados',
-      width: '200',
+      width: '250',
       // height: '183px',
-      description: '<p>Este projeto tem o objetivo de trazer funcionalidades que beneficiam gestão da produçao e o controle de processos</p> <p>Com esse sistema, é possível cadastrar os itens e seus roteiros de produção, gerar os romaneios de inspeção, ordens de produção e emitir relatórios.</p>',
+      description: '<p>Este projeto tem o objetivo de trazer funcionalidades que beneficiam a gestão da produçao e o controle de processos.</p> <p>Com esse sistema, é possível cadastrar os itens e seus roteiros de produção, gerar os romaneios de inspeção, ordens de produção e emitir relatórios.</p>',
       links: [
         {
           name: 'Conheça a Heimd4ll',
@@ -32,6 +32,21 @@ export class ProjectsComponent {
     },
 
     // INSERIR MAIS PROJETOS AQUI
+
+    {
+      src: '/assets/img/portfolio/project-heimd4ll2.png',
+      alt: 'Projeto Heimd4ll',
+      title: 'Heimd4ll - Inteligência em Dados',
+      width: '250',
+      // height: '183px',
+      description: '<p>Este projeto tem o objetivo de trazer funcionalidades que beneficiam gestão da produçao e o controle de processos</p> <p>Com esse sistema, é possível cadastrar os itens e seus roteiros de produção, gerar os romaneios de inspeção, ordens de produção e emitir relatórios.</p>',
+      links: [
+        {
+          name: 'Conheça a Heimd4ll',
+          href: 'https://www.linkedin.com/company/heimd4ll/'
+        },
+      ]
+    },
 
 
   ]);
